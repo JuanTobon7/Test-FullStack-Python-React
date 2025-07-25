4f40c76c419120e4e06a25587af472f86fcf15b6
Juan_Tobon <jctobon11.2@gmail.com>
2025-07-24 21:14:42 -0500

    fix: Correct order of app.run and blueprint registration in server.py

--- api/server.py
+++ api/server.py
@@ -3,14 +3,15 @@ from flask_cors import CORS
 from app.db.session.session import SessionLocal
 from app.routes.index import api
 from app.config.config import Config
-
+import os
 app = Flask(__name__)
 
 CORS(app)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render define PORT, si no está usa 5000
     app.config.from_object(Config)
-    
+    app.run(host='0.0.0.0', port=port)
     app.register_blueprint(api, url_prefix='/api')
     
     @app.before_request
